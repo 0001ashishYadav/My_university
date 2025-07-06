fdad8a650891bcca3d0a03cea2e5d4499d7822bc
000ashishYadav <000ashishyadav2003@gmail.com>
2025-07-06 13:36:39 +0530

    all set

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Academics from "./pages/Academic";
+import Admissions from "./pages/Admission";
+import StudentLife from "./pages/StudentLife";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <>hello</>;
+  return (
+    <Router>
+      <div className="App min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/academics" element={<Academics />} />
+            <Route path="/admissions" element={<Admissions />} />
+            <Route path="/student-life" element={<StudentLife />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
